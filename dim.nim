449e1dfcae53a4da0fc974a7b4f8f13f45b63539
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2019-08-24 22:02:34 +0200

    convert dim level value to float

--- dim.nim
+++ dim.nim
@@ -1,16 +1,23 @@
-import os, strutils, streams, parseutils, system, strformat
+import os, strutils, streams, parseutils, system, strformat, math
+
+const MAX_LEVEL = 10
+const LIGHT = [   1,    1,    1,    1,    5,    5,   10,   25,   50,  100,  100]
+const TEMP =  [1500, 1500, 1500, 1500, 2000, 3000, 4000, 4500, 5500, 5500, 6500]
+const DIM =   [   0,   50,   75,  100]
 
 
-const MAX_LEVEL = 9
-const LIGHT = [   1,    1,    1,    5,    5,   10,   25,   50,  100]
-const TEMP =  [1500, 1500, 1500, 2000, 3000, 4000, 4500, 5500]
-const DIM =   [  50,   75,  100]
-
-
-proc get_value_for_level(arr: openArray[int], level: int): int =
-  assert(level >= 0)
-  let idx = min(len(arr) - 1, level)
-  arr[idx]
+proc get_value_for_level(arr: openArray[int], level: float): int =
+  assert(level >= 0.0)
+  if floor(level) == level:
+    let idx = min(len(arr) - 1, int(level))
+    return arr[idx]
+  else:
+    let idx1 = min(len(arr) - 1, int(floor(level)))
+    let idx2 = min(len(arr) - 1, int(ceil(level)))
+    let v1 = float(arr[idx1])
+    let v2 = float(arr[idx2])
+    let a = level - floor(level)
+    return int(v1 + a * (v2 - v1))
 
 
 proc system(cmd: string) =
@@ -28,7 +35,7 @@ proc set_light(level: int) =
 
 proc print_usage() =
   echo "Usage: dim LEVEL\n"
-  echo "LEVEL must be an integer between 1 and 7.\n"
+  echo fmt"LEVEL must be a float between 0 and {MAX_LEVEL}.\n"
 
 
 proc set_redshift(temp: int, dim: int) =
@@ -42,18 +49,16 @@ if paramCount() == 0:
   set_light(50)
 
 elif paramCount() == 1:
-  var level: int
-  let res = parseInt(paramStr(1), level)
+  var level: float
+  let res = parsefloat(paramStr(1), level)
 
   if res == 0:
     print_usage()
     quit(QuitFailure)
 
-  if level < 1 or level > MAX_LEVEL:
+  if level < 0 or level > MAX_LEVEL:
     print_usage()
     quit(QuitFailure)
-
-  level -= 1
 
   let light = get_value_for_level(LIGHT, level)
   let temp = get_value_for_level(TEMP, level)
