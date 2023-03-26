69d4e05631f16c755fc6bda48adeaa44c96a389b
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2023-03-26 13:58:54 +0200

    increment

--- ff.c
+++ ff.c
@@ -1,14 +1,16 @@
-#include "wst.h"
+#include "grv.h"
 
 int main(int argc, char** argv)
 {
-    char* cmd = "firefox --new-window";
+    grv_str ff_cmd = grv_str_new("firefox --new-window");
 
-    if (argc > 1)
-        for (int i = 1; i < argc; ++i)
-            system(wst_string_join(cmd, argv[i], " "));
-    else
-        system(cmd);
-
+    if (argc > 1) {
+        grv_strarr args = grv_strarr_from_cstr_array(argv + 1, argc - 1);
+        grv_strarr_push_front(&args, &ff_cmd);
+        grv_str cmd = grv_strarr_join(&args, " ");
+        grv_system(&cmd);
+    } else {
+        grv_system(&ff_cmd);
+    }
     return 0;
 }
--- gmail.c
+++ gmail.c
@@ -1,4 +1,4 @@
-#include "wst.h"
+#include <stdlib.h>
 
 int main()
 {
