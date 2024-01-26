4137e0777e6036e987e25a1a5fa0f7e1590ea825
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2024-01-26 18:18:24 +0100

    fix status

--- status/status_time.c
+++ status/status_time.c
@@ -2,10 +2,9 @@
 #include "wst.h"
 #include "time.h"
 #include "status_format.h"
-#include "grv.h"
+#include "grv/grv_str.h"
 
-grv_str format_day_of_month(struct tm* t) {
-    grv_str time_str = {0};
+grv_str_t format_day_of_month(struct tm* t) {
     s32 day = t->tm_mday;
     char* suffix = 
         day == 1 || day == 21 || day == 31 ? "st" :
@@ -13,39 +12,34 @@ grv_str format_day_of_month(struct tm* t) {
         day == 3 || day == 23 ? "rd" :
         "th";
     
-    grv_str result = grv_str_from_s32(day);
-    grv_str_append_cstr(&result, suffix);
-    return result;
+    return grv_str_new_with_format("%d%s", day, suffix);
 }
 
-grv_str format_day_of_week(struct tm* t) {
-    grv_str day = {0}; 
-    size_t len = strftime(grv_str_cstr(&day), grv_str_get_capacity(&day), "%a", t);
-    grv_str_set_size(&day, len); 
-    return day;
+grv_str_t format_day_of_week(struct tm* t) {
+    char buffer[64];
+    size_t len = strftime(buffer, 64, "%a", t);
+    return grv_str_new(buffer);
 }
 
-grv_str format_hours_minutes(struct tm* t) {
-    grv_str time_str = {0};
-    size_t len = strftime(grv_str_cstr(&time_str), grv_str_get_capacity(&time_str), "%H:%M", t);
-    grv_str_set_size(&time_str, len);
-    return time_str;
+grv_str_t format_hours_minutes(struct tm* t) {
+    char buffer[64];
+    size_t len = strftime(buffer, 64, "%H:%M", t);
+    return grv_str_new(buffer);
 }
-
 
 char* formatCurrentTime() {
     time_t now = time(0);
     struct tm* t = localtime(&now);
 
-    grv_str hm = format_hours_minutes(t);
-    grv_str dom = format_day_of_month(t);
-    grv_str dow = format_day_of_week(t);
+    grv_str_t hm = format_hours_minutes(t);
+    grv_str_t dom = format_day_of_month(t);
+    grv_str_t dow = format_day_of_week(t);
 
-    grv_str timestr = grv_str_copy(&dow);
+    grv_str_t timestr = grv_str_copy(dow);
     grv_str_append_char(&timestr, ' ');
-    grv_str_append(&timestr, &dom);
+    grv_str_append(&timestr, dom);
     grv_str_append_char(&timestr, ' ');
-    grv_str_append(&timestr, &hm);
+    grv_str_append(&timestr, hm);
 
     grv_str_free(&hm);
     grv_str_free(&dom);
@@ -56,7 +50,7 @@ char* formatCurrentTime() {
         hour >= 23 || hour < 5 ? COLOR_RED 
         : hour >= 22 ? COLOR_YELLOW 
         : COLOR_WHITE; 
-    char* result = formatStatusField(grv_str_cstr(&timestr), color);
+    char* result = formatStatusField(grv_str_cstr(timestr), color);
     
     grv_str_free(&timestr);
 
