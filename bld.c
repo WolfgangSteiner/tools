d66d1bae7575a882bf0a3d7c91bd51d1752b256d
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2024-04-16 19:25:26 +0200

    adapt code to moved grv

--- bld.c
+++ bld.c
@@ -1,33 +1,37 @@
-#include "grv/grvbld.h"
+#include "lib/grv/grvbld.h"
 
 static grvbld_config_t* config = 0;
+static grvbld_target_t* libgrv = 0;
 
 grvbld_config_t* create_config(int argc, char** argv) {
     grvbld_config_t* config =  grvbld_config_new(argc, argv);
-    grvbld_config_add_include_directory(config, "grv/include");
+    grvbld_config_add_include_directory(config, "lib/grv/include");
     grvbld_config_add_include_directory(config, ".");
     return config;
 }
 
-grvbld_target_t* make_target(char* name) {
-    grvbld_target_t* target = grvbld_target_create(name, GRVBLD_EXECUTABLE);
-    grvbld_target_add_src(target, "grv/src/grv.c");
-    return target;
-}
-
 void build_exe(char* name) {
-    grvbld_target_t* target = make_target(name);
+    grvbld_target_t* target = grvbld_target_create_executable(name);
     char* src = grvbld_cstr_cat(name, ".c");
     grvbld_target_add_src(target, src);
+    grvbld_target_link(target, libgrv);
     grvbld_build_target(config, target);
+}
+
+void build_grv(void) {
+    libgrv = grvbld_target_create_static_library("grv");
+    grvbld_target_add_src(libgrv, "lib/grv/src/grv.c");
+    grvbld_build_target(config, libgrv);
 }
 
 int main(int argc, char** argv) {
     GRV_CHECK_AND_REBUILD();    
     config = create_config(argc, argv);
+    build_grv();
 
-    grvbld_target_t* status = make_target("status");
+    grvbld_target_t* status = grvbld_target_create_executable("status");
     grvbld_target_add_src(status, "status/status.c");
+    grvbld_target_link(status, libgrv);
     grvbld_build_target(config, status);
 
     build_exe("battery_watch");
