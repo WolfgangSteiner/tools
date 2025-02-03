967a5a0bb69bac80d30cedd9439e3133f192d0c3
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2025-02-03 18:01:06 +0100

    update sanitize_file_names to rename recursively

--- scripts/sanitize_file_names.py
+++ scripts/sanitize_file_names.py
@@ -1,37 +1,50 @@
 #! /usr/bin/env python
-import re, os, sys
+import glob, re, os, sys
+import typer
 
-dry_run = False
-shorten_dates = False
 
-if len(sys.argv) > 1:
-    if sys.argv[1] in ("-n", "--dry-run"):
-        dry_run = True
-        sys.argv.pop(1)
-    elif sys.argv[1] in ("--shorten-dates"):
-        shorten_dates = True
-        sys.argv.pop(1)
+def construct_new_filename(filename, shorten_dates):
+    path, filename = os.path.split(filename)
 
-for filename in os.listdir("."):
-    old_path = os.path.join(".", filename)
-    
-    # Check if it's a file (not a directory)
-    if True or os.path.isfile(filename):
-        nfn = filename.replace(' - ', '_')
-        nfn = re.sub(r'([a-z])([A-Z])', r'\1_\2', nfn)
-        nfn = re.sub(r'(\d{4})-(\d{2})(?:-(\d{2}))?', r'\1\2\3', nfn)
-        if shorten_dates:
-            nfn = re.sub(r'20(\d{2})(\d{2})(\d{2})?', r'\1\2\3', nfn)
-        nfn = nfn.lower() 
-        nfn = re.sub(r'[ -]', '_', nfn)
-        nfn = re.sub(r'__+', '_', nfn)
-        nfn = re.sub(r'[^a-zäöü0-9_.]', '', nfn)
-        if filename != nfn:
-            if dry_run:
-                print(f"'{filename}' -> '{nfn}'")
-            else:
-                try:
-                    os.rename(filename, nfn)
-                    print(f"'{filename}' -> '{nfn}'")
-                except Exception as e:
-                    print(f"Failed to rename '{filename}': {e}")
+    fn = filename.replace(' - ', '_')
+    fn = re.sub(r'([a-z])([A-Z])', r'\1_\2', fn)
+    fn = re.sub(r'(\d{4})-(\d{2})(?:-(\d{2}))?', r'\1\2\3', fn)
+    if shorten_dates:
+        fn = re.sub(r'20(\d{2})(\d{2})(\d{2})?', r'\1\2\3', fn)
+    fn = fn.lower() 
+    fn = re.sub(r'[ -]', '_', fn)
+    fn = re.sub(r'__+', '_', fn)
+    fn = re.sub(r'[^a-zäöü0-9_.]', '', fn)
+    return os.path.join(path, fn)
+
+def get_path_list(directory=".", recursive=False):
+    if recursive:
+        return glob.glob(os.path.join(directory, "**"), recursive=True)
+    else:
+        return glob.glob("*")
+             
+def main(
+    dry_run:bool = False,
+    shorten_dates:bool = False,
+    recursive:bool = False,
+    rename_directories:bool = True,
+):
+    file_list = get_path_list(".", recursive=recursive)
+    print(file_list)
+    file_list = sorted(file_list, key=lambda p: len(p.split('/')), reverse=True)
+    for filename in file_list:
+        if rename_directories or os.path.isfile(filename):
+            new_filename = construct_new_filename(filename, shorten_dates)
+            if filename != new_filename:
+                if dry_run:
+                    print(f"'{filename}' -> '{new_filename}'")
+                else:
+                    try:
+                        os.rename(filename, new_filename)
+                        print(f"'{filename}' -> '{new_filename}'")
+                    except Exception as e:
+                        print(f"Failed to rename '{filename}': {e}")
+
+
+if __name__ == "__main__":
+    typer.run(main)
